3fc79f981dc878b6f1e01cf7dccc01abf8c85aea
zhangzh <zhangzh@deepblueai.com>
2021-01-06 21:22:57 +0800

    nothing modified

--- test/test.go
+++ test/test.go
@@ -5,23 +5,5 @@
 **/
 package main
 
-import (
-	"fmt"
-	"github.com/scottkiss/grtm"
-	"time"
-)
-
 func main() {
-	gm := grtm.NewGrManager()
-	gm.NewLoopGoroutine("myfunc", myfunc)
-	fmt.Println("main function")
-	time.Sleep(time.Second * time.Duration(40))
-	fmt.Println("stop myfunc goroutine")
-	gm.StopLoopGoroutine("myfunc")
-	time.Sleep(time.Second * time.Duration(80))
-}
-
-func myfunc() {
-	fmt.Println("do something repeat by interval 4 seconds")
-	time.Sleep(time.Second * time.Duration(4))
 }
