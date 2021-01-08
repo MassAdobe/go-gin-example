8273262b4d52b81fd21e9ed96d29293590de7668
zhangzh <zhangzh@deepblueai.com>
2021-01-08 15:38:47 +0800

    test ratelimit

--- test/test.go
+++ test/test.go
@@ -5,18 +5,5 @@
 **/
 package main
 
-import (
-	"fmt"
-)
-
 func main() {
-	chanx := make(chan bool)
-	close(chanx)
-
-	if _, ok := <-chanx; ok {
-		fmt.Println("已经关闭")
-	} else {
-		fmt.Println("还没关闭")
-	}
-
 }
